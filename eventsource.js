a852133f8c9f57b9389c770c31aabb553858b5b2
Myles Eftos <myles@madpilot.com.au>
2016-09-30 10:46:33 +1000

    Initial check in

---
+++ eventsource.js
@@ -0,0 +1,36 @@
+var EventSource = require('eventsource');
+
+module.exports = function(RED) {
+    function EventSourceOutNode(config) {
+      RED.nodes.createNode(this, config);
+      
+      var es = new EventSource(config.url, { rejectUnauthorized: false });
+
+      var context = this;
+
+      es.addEventListener("error", function(error) {
+        context.error("Event Source error", error);
+        context.status({ fill: "red", shape: "ring", text: "Error" });  
+      });
+
+      es.addEventListener("open", function() {
+        context.status({ fill: "green", shape: "ring", text: "Connected" });  
+      });
+
+      es.addEventListener("message", function(msg) {
+        var payload = msg.data;
+        try {
+          payload = JSON.parse(msg.data);
+        } catch(err) {};
+
+        context.send({ payload: payload });
+      });
+
+      this.on('close', function() {
+        es.close();
+        context.status({ fill: "red", shape: "ring", text: "Disconnected" });  
+      });
+    }
+    
+    RED.nodes.registerType("eventsource", EventSourceOutNode);
+}
